4942feab7799f05fe3e74698e01c375738e8707d
Ismail Abu Hassanein <iabuhassanein@gmail.com>
2021-12-06 14:50:10 +0200

    IT-1

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule); 
 
 // TODO
 // import { platformBrowser }    from '@angular/platform-browser';
